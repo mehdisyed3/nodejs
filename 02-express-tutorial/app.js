907d881e120bb6f7d94807e8e8e7013826c4d7d1
Syed Mehdi <mehdisyed3@gmail.com>
2023-11-28 19:19:51 -0500

    tutorial commit

--- 02-express-tutorial/app.js
+++ 02-express-tutorial/app.js
@@ -1,1 +1,29 @@
-console.log('Express Tutorial')
+// const http = require('http')
+
+// const server = http.createServer((req,res)=>{
+//   console.log('user hit the server')
+//   if (req.url === '/about'){
+    
+//     res.writeHead(200, {'content-type':'text/html'})
+//     res.write('<p>this is the about page .. thanks for visiting.. will see you soon on the next page</p>')
+//     res.end()
+//   }
+//   else if (req.url === '/'){ 
+
+//     res.writeHead(200,{'content-type':'text/html'})
+//     res.write('<h4> THIS IS THE HOME PAGE</h4>')
+//     res.end()
+//   }
+//   else{
+//     res.writeHead(404,{'content-type':'text/html'})
+//     res.write('<h4> THIS IS doesnt exist</h4>')
+//     res.end()
+  
+//   }
+// })
+
+// server.listen(5000)
+
+
+// VIDEO STOPPED @ 4:30
+
