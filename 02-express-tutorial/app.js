1a657203b61cc588239543e6f07e663f57ed6b15
Syed Mehdi <mehdisyed3@gmail.com>
2023-11-30 16:30:28 -0500

    learned how to serve pages/content via node

--- 02-express-tutorial/app.js
+++ 02-express-tutorial/app.js
@@ -1,29 +1,63 @@
-// const http = require('http')
+const http = require('http')
+const {readFileSync} = require('fs')
 
-// const server = http.createServer((req,res)=>{
-//   console.log('user hit the server')
-//   if (req.url === '/about'){
+
+// get all files
+const homePage = readFileSync('./navbar-app/index.html')
+const styles = readFileSync('./navbar-app/styles.css')
+const homeJavaScript = readFileSync('./navbar-app/browser-app.js')
+const homeLogo  = readFileSync('./navbar-app/logo.svg')
+
+
+const server = http.createServer((req,res)=>{
+  console.log('user hit the server')
+  if (req.url === '/about'){
     
-//     res.writeHead(200, {'content-type':'text/html'})
-//     res.write('<p>this is the about page .. thanks for visiting.. will see you soon on the next page</p>')
-//     res.end()
-//   }
-//   else if (req.url === '/'){ 
+    res.writeHead(200, {'content-type':'text/html'})
+    res.write('<p>this is the about page .. thanks for visiting.. will see you soon on the next page</p>')
+    
+    res.end()
+  }
+  else if (req.url === '/'){ 
 
-//     res.writeHead(200,{'content-type':'text/html'})
-//     res.write('<h4> THIS IS THE HOME PAGE</h4>')
-//     res.end()
-//   }
-//   else{
-//     res.writeHead(404,{'content-type':'text/html'})
-//     res.write('<h4> THIS IS doesnt exist</h4>')
-//     res.end()
+    res.writeHead(200,{'content-type':'text/html'})
+    // res.write('<h4> THIS IS THE HOME PAGE</h4>')
+    res.write(homePage)
+    
+    res.end()
+  }
+  else if (req.url === '/styles.css'){ 
+
+    res.writeHead(200,{'content-type':'text/css'})
+    res.write(styles)
+    
+    res.end()
+  }
+  else if (req.url === '/logo.svg'){ 
+
+    res.writeHead(200,{'content-type':'image/svg+xml'})
+    res.write(homeLogo)
+    
+    res.end()
+  }
+  else if (req.url === '/browser-app.js'){ 
+
+    res.writeHead(200,{'content-type':'text/javascript'})
+    res.write(homeJavaScript)
+    
+    res.end()
+  }
+  else{
+    res.writeHead(404,{'content-type':'text/html'})
+    res.write('<h4> THIS IS doesnt exist</h4>')
+    res.end()
   
-//   }
-// })
+  }
+})
 
-// server.listen(5000)
+server.listen(5000)
 
 
-// VIDEO STOPPED @ 4:30
+
+
 
