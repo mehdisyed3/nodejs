f56e1f04679a759302ad691c3c025a559ac62bda
Syed Mehdi <mehdisyed3@gmail.com>
2023-12-23 00:50:13 -0500

    send static data using app.use

--- 02-express-tutorial/app.js
+++ 02-express-tutorial/app.js
@@ -1,21 +1,21 @@
 const express = require('express')
+const path = require('path')
 const app = express()
 
-app.get('/',(req,res)=>{
-  res.status(200).send('<h1>this is a home page</h1>')
+app.use(express.static('./navbar-app'))
+
+// app.get('/',(req,res)=>{
+// res.status(200).sendFile(path.resolve(__dirname,'./navbar-app/index.html'))
+// })
+
+app.all('*',(req,res) =>{
+  res.status(404).send('This page does not exist')
 })
 
-app.get('/about',(req,res)=>{
-  res.status(200).send('<h1>this is the about page</h1>')
+
+app.listen(5000,()=>{
+  console.log('server is being listened on port 5000')
 })
 
-app.get('*',(req,res)=>{
-  res.status(404).send('<h1>this page does not exist</h1>')
-})
-
-
-app.listen(5000, ()=>{
-  console.log("app is being listened on 5000")
-})
-
-// WAtched till 5 hours
+// followed video till 5:24 .. when starting again start from 5:20..
+// there are 2 aspects of backend.. ssr and api.. next i am going to learn about apis where ill be sending json data upon request
