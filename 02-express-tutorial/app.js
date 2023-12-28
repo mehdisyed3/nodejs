931032eb603c849807502bf0a9e4a091be10b5ec
Syed Mehdi <mehdisyed3@gmail.com>
2023-12-27 23:27:22 -0500

    worked on queries and params request

--- 02-express-tutorial/app.js
+++ 02-express-tutorial/app.js
@@ -1,21 +1,66 @@
-const express = require('express')
-const path = require('path')
-const app = express()
-
-app.use(express.static('./navbar-app'))
+// const express = require('express')
+// const app = express()
+// const {products, people} = require('./data')
 
 // app.get('/',(req,res)=>{
-// res.status(200).sendFile(path.resolve(__dirname,'./navbar-app/index.html'))
+//   res.send('<h1>thiis is the home page</h1><a href="api/product">Product</a>')
+//   // res.json(products)
+// })
+// app.get('/api/product',(req,res)=>{
+//   const newProduct = products.map(item =>{
+//     const {id,name,image} = item
+//     return {
+//       id,
+//       name,
+//       image
+//     }
+//   })
+//   res.json(newProduct)
+
 // })
 
-app.all('*',(req,res) =>{
-  res.status(404).send('This page does not exist')
-})
+// app.get('/api/product/:id',(req,res)=>{
+//   const {id} = req.params
+//    const singleProduct = products.find(item => item.id === Number(id))
 
+//    if(!singleProduct){
+//    return res.status(404).send('<p>Product does not exist</p>')
+//    }
+//    else{
 
-app.listen(5000,()=>{
-  console.log('server is being listened on port 5000')
-})
+//      return res.json(singleProduct)
+//    }
 
-// followed video till 5:24 .. when starting again start from 5:20..
-// there are 2 aspects of backend.. ssr and api.. next i am going to learn about apis where ill be sending json data upon request
+// })
+
+// app.get('/api/v1/query',(req,res)=>{
+//   const {search, limit} = req.query
+//   let requestedProduct = [...products]
+
+//   if(search){
+//     requestedProduct = requestedProduct.filter(item=>{
+//       return item.name.startsWith(search)
+//     })
+//   }
+//   if(limit){
+//     requestedProduct = requestedProduct.slice(0,Number(limit))
+//   }
+
+//   if(requestedProduct.length < 1){
+//     res.status(200).send('sorry there was nothing matching your request')
+//   }
+//   else{
+
+//     res.status(200).json(requestedProduct)
+//   }
+
+// })
+
+// app.get('/people',(req,res)=>{
+//   res.json(people)
+// })
+// app.listen(5000,()=>{
+//   console.log('app is being listened on port 5000')
+// })
+
+// // watchd vdeo till 5:50 .. its now getting interesting.. 
