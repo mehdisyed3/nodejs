d05790958eeec438303e2bf6a3b1460cb451552c
Syed Mehdi <mehdisyed3@gmail.com>
2024-01-08 16:27:33 -0500

    update

--- 02-express-tutorial/app.js
+++ 02-express-tutorial/app.js
@@ -4,7 +4,9 @@ let {people} = require('./data')
 
 app.use(express.static('./methods-public'))
 
-// app.use(express.urlencoded({extended:false}))
+app.use(express.urlencoded({extended:false}))
+
+app.use(express.json())
 
 app.get('/api/people', (req,res)=>{
   res.status(200).json({success:true,data:people})
@@ -16,7 +18,23 @@ app.post('/login',(req,res)=>{
   if(name){
     return res.status(200).send(`Welcome ${name}`)
   }else{
-    return res.status(401).setDefaultEncoding('please send credeintials')
+    return res.status(401).send('please send credeintials')
+  }
+
+})
+
+
+
+app.post('/api/people',(req,res)=>{
+  console.log(req.body)
+  const {name} = req.body
+
+ 
+  if(!name){
+    return res.status(400).json({success:false, message:'please Provide name'})
+   
+  }else{
+    return res.status(201).json({success:true, person:name})
   }
 
 })
@@ -25,4 +43,4 @@ app.listen(5000,()=>{
   console.log('app is being listened on port 5000')
 })
 
-// watched video tilll 6:28
+// watched video tilll 7:20
