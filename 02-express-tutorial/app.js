7ebe19f9d8c5ca354f7f11c56dfb64021a6ffd1d
Syed Mehdi <mehdisyed3@gmail.com>
2024-01-09 14:07:24 -0500

    end of post

--- 02-express-tutorial/app.js
+++ 02-express-tutorial/app.js
@@ -1,77 +1,19 @@
 const express = require('express')
+const  people  = require('./routes/people')
+const auth = require('./routes/auth')
 const app = express()
-let {people} = require('./data')
+
 
 app.use(express.static('./methods-public'))
 
 app.use(express.urlencoded({extended:false}))
 
 app.use(express.json())
-
-app.get('/api/people', (req,res)=>{
-  res.status(200).json({success:true,data:people})
-})
-
-app.post('/login',(req,res)=>{
-  console.log(req.body)
-  const {name} = req.body
-  if(name){
-    return res.status(200).send(`Welcome ${name}`)
-  }else{
-    return res.status(401).send('please send credeintials')
-  }
-
-})
-
-app.post('/api/people',(req,res)=>{
-  console.log(req.body)
-  const {name} = req.body
-
- 
-  if(!name){
-    return res.status(400).json({success:false, message:'please Provide name'})
-   
-  }else{
-    return res.status(201).json({success:true, person:name})
-  }
-
-})
-
-app.post('/api/postman/people',(req,res)=>{
-  const {name} = req?.body
-
-  if(!name){
-   return res.status(400).json({success:false, message:" PLEASE PROVIDE A NAME "})
-  }else{
-    return res.status(201).json({success:true, data:[...people, {id:Math.random(),name}]})
-  }
-
-})
-
-app.put('/api/people/:id',(req,res)=>{
-  const {name} = req?.body
-  const {id}= req.params
-
-  let updated = people.map(item =>{
-    if(item.id === Number(id)){
-       item.name = name
-    }
-    return item
-  })
-
-  res.status(201).json({succes:true, data:updated})
-  
+app.use('/api/people',people)
+app.use('/login',auth)
 
 
-  console.log('>>> helllo')
 
-  // if(!name){
-  //  return res.status(400).json({success:false, message:" PLEASE PROVIDE A NAME "})
-  // }else{
-  //   return res.status(201).json({success:true, data:[...people, {id:Math.random(),name}]})
-  // }
-
-})
 
 
 
